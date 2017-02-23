213b0ecd78c10d6f5a9fc3ea50c351185f8a60b3
mrbean-bremen <andreas.bohne@mevis.de>
2017-02-23 20:33:45 +0100

    Python 3 support: adapt imports for Python 3 (untested)

--- django_windows_tools/service.py
+++ django_windows_tools/service.py
@@ -36,11 +36,14 @@ import win32con
 import win32file
 from multiprocessing import Process
 from multiprocessing.util import get_logger
-import multiprocessing.forking
-import logging
-import ConfigParser
+
 import ctypes
 import traceback
+
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
 
 # is using virtualenv under Windows, the sys.exec_prefix used in forking is set the the base directory
 # of the virtual environment, not the Scripts subdirectory where the python executable resides
@@ -49,65 +52,84 @@ if hasattr(sys, 'real_prefix') and sys.platform == 'win32':
 
 GenerateConsoleCtrlEvent = ctypes.windll.kernel32.GenerateConsoleCtrlEvent
 
-old_get_preparation_data = multiprocessing.forking.get_preparation_data
+try:
+    import multiprocessing.forking as forking
+    main_path_key = 'main_path'
+except ImportError:
+    import multiprocessing.spawn as forking
+    main_path_key = 'init_main_from_path'
 
 # Monkey patch the Windows Process implementation to avoid thinking
 # That 'PythonService.exe' is a python script
+old_get_preparation_data = forking.get_preparation_data
+
+
 def new_get_preparation_data(name):
     d = old_get_preparation_data(name)
-    if d.has_key('main_path') and d['main_path'].lower().endswith('.exe'):
-        del d['main_path']
+    if main_path_key in d and d[main_path_key].lower().endswith('.exe'):
+        del d[main_path_key]
     return d
 
-multiprocessing.forking.get_preparation_data = new_get_preparation_data
-    
+
+forking.get_preparation_data = new_get_preparation_data
+
 # Do the same monkey patching on billiard which is a fork of
 # multiprocessing
 try:
     import billiard.forking as billiard_forking
-    billiard_old_get_preparation_data = billiard_forking.get_preparation_data
+    main_path_key = 'main_path'
+except ImportError:
+    try:
+        import billiard.spawn as billiard_forking
+        main_path_key = 'init_main_from_path'
 
-    def billiard_new_get_preparation_data(name):
-        d = billiard_old_get_preparation_data(name)
-        if d.has_key('main_path') and d['main_path'].lower().endswith('.exe'):
-            #del d['main_path']
-            d['main_path'] = '__main__.py'
-        return d
-        
-    billiard_forking.get_preparation_data = billiard_new_get_preparation_data
-except:
-    pass
-    
+        billiard_old_get_preparation_data = billiard_forking.get_preparation_data
+
+        def billiard_new_get_preparation_data(name):
+            d = billiard_old_get_preparation_data(name)
+            if main_path_key in d and d[main_path_key].lower().endswith('.exe'):
+                # del d['main_path']
+                d[main_path_key] = '__main__.py'
+            return d
+
+        billiard_forking.get_preparation_data = billiard_new_get_preparation_data
+    except:
+        pass
+
 
 def log(msg):
     '''Log a message in the Event Viewer as an informational message'''
     import servicemanager
     servicemanager.LogInfoMsg(str(msg))
 
+
 def error(msg):
     '''Log a message in the Event Viewer as an error message'''
     import servicemanager
     servicemanager.LogErrorMsg(str(msg))
-    
+
+
 def initialize_logger(config):
     class StdErrWrapper:
         """
             Call wrapper for stderr
         """
+
         def write(self, s):
             get_logger().info(s)
+
     import logging
-    
+
     logger = get_logger()
     values = dict(
-        format = '[%(levelname)s/%(processName)s] %(message)s',
-        filename = None,
-        level = 'INFO',
+        format='[%(levelname)s/%(processName)s] %(message)s',
+        filename=None,
+        level='INFO',
     )
     if config and config.has_section('log'):
-        for (name,value) in config.items('log'):
+        for (name, value) in config.items('log'):
             values[name] = value
-        
+
     if values['filename']:
         formatter = logging.Formatter(values['format'])
         handler = logging.FileHandler(values['filename'])
@@ -115,7 +137,7 @@ def initialize_logger(config):
         logger.addHandler(handler)
         logger.setLevel(getattr(logging, values['level'].upper(), logging.INFO))
         sys.stderr = StdErrWrapper()
-    
+
 
 def start_django_command(config, args):
     '''
@@ -123,18 +145,19 @@ def start_django_command(config, args):
     
     This commands is supposed to start in a spawned (child process).
     It tries to import the settings of the project before handling the command.
-    '''    
+    '''
     initialize_logger(config)
 
     log('Starting command : %s' % ' '.join(args))
     get_logger().info('Starting command : %s' % ' '.join(args))
     from django.core.management import execute_from_command_line
-    
+
     try:
         execute_from_command_line(args)
     except:
         error('Exception occured : %s' % traceback.format_exc())
-    
+
+
 def spawn_command(config, server_name):
     '''
     Spawn a command specified in a configuration file and return the process object.
@@ -157,17 +180,19 @@ def start_commands(config):
     services = config.get(node_name, 'run') if config.has_section(node_name) else config.get('services', 'run')
     for server_name in services.split():
         processes.append(spawn_command(config, server_name))
-    
+
     return processes
-    
+
+
 def end_commands(processes):
     '''
     Terminate all the processes in the specified array.
     '''
     for process in processes:
-        #GenerateConsoleCtrlEvent(1, process.pid)
+        # GenerateConsoleCtrlEvent(1, process.pid)
         process.terminate()
         process.join()
+
 
 def test_commands(base_path=None, timeout=10):
     '''
@@ -179,7 +204,8 @@ def test_commands(base_path=None, timeout=10):
     import time
     time.sleep(timeout)
     end_commands(processes)
-    
+
+
 def get_config_modification_handle(path=None):
     '''Returns a Directory change handle on the configuration directory.
     
@@ -188,8 +214,8 @@ def get_config_modification_handle(path=None):
     '''
     if not path:
         path = dirname(abspath(__file__))
-		
-    change_handle = win32file.FindFirstChangeNotification (
+
+    change_handle = win32file.FindFirstChangeNotification(
         path,
         0,
         win32con.FILE_NOTIFY_CHANGE_LAST_WRITE
@@ -209,7 +235,8 @@ def read_config(base_path=None, filename='service.ini'):
     log(path)
     config.read(path)
     return config
-    
+
+
 class DjangoService(win32serviceutil.ServiceFramework):
     """NT Service."""
 
@@ -217,7 +244,7 @@ class DjangoService(win32serviceutil.ServiceFramework):
     _svc_display_name_ = "Django Background Processes"
     _svc_description_ = "Run the Django background Processes"
     _config_filename = 'service.ini'
-    
+
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
         log('Initialization')
@@ -227,7 +254,7 @@ class DjangoService(win32serviceutil.ServiceFramework):
         initialize_logger(self.config)
         if not self._base_path in sys.path:
             sys.path.append(self._base_path)
-           
+
         parent_path = dirname(self._base_path)
         if not parent_path in sys.path:
             sys.path.append(parent_path)
@@ -237,10 +264,10 @@ class DjangoService(win32serviceutil.ServiceFramework):
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
         log('starting')
         self.ReportServiceStatus(win32service.SERVICE_RUNNING)
-        
+
         self.modification_handle = get_config_modification_handle(self._base_path)
         self.configuration_mtime = os.stat(os.path.join(self._base_path, self._config_filename)).st_mtime
-        
+
         keep_running = True
         do_start = True
         while keep_running:
@@ -248,16 +275,17 @@ class DjangoService(win32serviceutil.ServiceFramework):
             # do the actual start
             if do_start:
                 self.start()
-            
+
             log('Started. Waiting for stop')
-            index = win32event.WaitForMultipleObjects([self.stop_event, self.modification_handle], False, win32event.INFINITE)
+            index = win32event.WaitForMultipleObjects([self.stop_event, self.modification_handle], False,
+                                                      win32event.INFINITE)
             if index == 0:
                 # The stop event has been signaled. Stop execution.
                 keep_running = False
             else:
-				# re-initialise handle
+                # re-initialise handle
                 win32file.FindNextChangeNotification(self.modification_handle)
-				
+
                 new_mtime = os.stat(os.path.join(self._base_path, self._config_filename)).st_mtime
                 if new_mtime != self.configuration_mtime:
                     self.configuration_mtime = new_mtime
@@ -267,7 +295,7 @@ class DjangoService(win32serviceutil.ServiceFramework):
                     self.config = read_config(self._base_path, self._config_filename)
                 else:
                     do_start = False
-                
+
         win32file.FindCloseChangeNotification(self.modification_handle)
 
     def SvcStop(self):
@@ -278,24 +306,25 @@ class DjangoService(win32serviceutil.ServiceFramework):
         log('Stopped')
         win32event.SetEvent(self.stop_event)
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
-        
+
     def start(self):
         self.processes = start_commands(self.config)
-        
+
     def stop(self):
         if self.processes:
             end_commands(self.processes)
             self.processes = []
         node_name = platform.node()
-        clean = self.config.get(node_name, 'clean') if self.config.has_section(node_name) else self.config.get('services', 'clean')
+        clean = self.config.get(node_name, 'clean') if self.config.has_section(node_name) else self.config.get(
+            'services', 'clean')
         if clean:
             for file in clean.split(';'):
                 try:
                     os.remove(file)
                 except:
                     error("Error while removing %s\n%s" % (file, traceback.format_exc()))
-            
-    
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         test_commands()
